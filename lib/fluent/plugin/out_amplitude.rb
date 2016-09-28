8fab3f5767c53c687d8fb4dc58de4d2bc687e427
Vijay Ramesh <vijay@change.org>
2016-09-27 17:28:51 -0700

    Initial plugin commit

---
+++ lib/fluent/plugin/out_amplitude.rb
@@ -0,0 +1,125 @@
+module Fluent
+  # Fluent::AmplitudeOutput plugin
+  class AmplitudeOutput < Fluent::BufferedOutput
+    Fluent::Plugin.register_output('amplitude', self)
+
+    include Fluent::HandleTagNameMixin
+
+    config_param :api_key, :string, secret: true
+    config_param :device_id_key, :string
+    config_param :user_id_key, :string
+    config_param :user_properties, :array, default: nil
+    config_param :event_properties, :array, default: nil
+    class AmplitudeError < StandardError
+    end
+
+    def initialize
+      super
+      require 'amplitude-api'
+    end
+
+    def configure(conf)
+      super
+      raise Fluent::ConfigError, "'api_key' must be specified." if @api_key.nil?
+
+      invalid = @device_id_key.nil? && @user_id_key.nil?
+      raise Fluent::ConfigError,
+            "'device_id_key' or 'user_id_key' must be specified." if invalid
+    end
+
+    def start
+      super
+      AmplitudeAPI.api_key = @api_key
+    end
+
+    def format(tag, time, record)
+      amplitude_hash = {
+        event_type: tag
+      }
+      amplitude_hash, record = extract_user_and_device(amplitude_hash, record)
+
+      unless amplitude_hash[:user_id] || amplitude_hash[:device_id]
+        raise AmplitudeError, 'Error: either user_id or device_id must be set'
+      end
+
+      amplitude_hash, record = extract_user_properties(amplitude_hash, record)
+
+      amplitude_hash = extract_event_properties(amplitude_hash, record)
+
+      [tag, time, amplitude_hash].to_msgpack
+    end
+
+    def write(chunk)
+      records = []
+      chunk.msgpack_each do |_tag, _time, record|
+        records << AmplitudeAPI::Event.new(simple_symbolize_keys(record))
+      end
+
+      send_to_amplitude(records)
+    end
+
+    private
+
+    def extract_user_and_device(amplitude_hash, record)
+      if @user_id_key && record[@user_id_key]
+        amplitude_hash[:user_id] = record.delete(@user_id_key)
+      end
+
+      if @device_id_key && record[@device_id_key]
+        amplitude_hash[:device_id] = record.delete(@device_id_key)
+      end
+
+      [amplitude_hash, record]
+    end
+
+    def extract_user_properties(amplitude_hash, record)
+      # if user_properties are specified, pull them off of the record
+      if @user_properties
+        amplitude_hash[:user_properties] = {}.tap do |user_properties|
+          @user_properties.each do |prop|
+            next unless record[prop]
+            user_properties[prop.to_sym] = record.delete(prop)
+          end
+        end
+      end
+      [amplitude_hash, record]
+    end
+
+    def extract_event_properties(amplitude_hash, record)
+      # if event_properties are specified, pull them off of the record
+      # otherwise, use the remaining record (minus any user_properties)
+      amplitude_hash[:event_properties] = begin
+        if @event_properties
+          record.select do |k, _v|
+            @event_properties.include?(k)
+          end
+        else
+          record
+        end
+      end
+      amplitude_hash
+    end
+
+    def send_to_amplitude(records)
+      log.info("sending #{records.length} to amplitude")
+      begin
+        res = AmplitudeAPI.track(records)
+        unless res.response_code == 200
+          raise "Got #{res.response_code} #{res.body} from AmplitudeAPI"
+        end
+      rescue StandardError => e
+        raise AmplitudeError, "Error: #{e.message}"
+      end
+    end
+
+    def simple_symbolize_keys(hsh)
+      Hash[hsh.map do |k, v|
+        begin
+          [k.to_sym, v]
+        rescue
+          [k, v]
+        end
+      end]
+    end
+  end
+end
---
+++ spec/out_amplitude_spec.rb
@@ -0,0 +1,147 @@
+require 'spec_helper'
+require 'amplitude-api'
+describe Fluent::AmplitudeOutput do
+  let(:amplitude_api) { double(:amplitude_api) }
+  let(:now) { Time.now.to_i }
+  let(:tag) { 'after_sign' }
+  let(:amplitude) do
+    Fluent::Test::BufferedOutputTestDriver.new(
+      Fluent::AmplitudeOutput.new
+    ).configure(conf)
+  end
+  let(:conf) do
+    %(
+      api_key XXXXXX
+      user_id_key user_id
+      device_id_key uuid
+      user_properties first_name, last_name
+      event_properties current_source
+    )
+  end
+
+  before do
+    Fluent::Test.setup
+    expect(AmplitudeAPI).to receive(:api_key=).with('XXXXXX')
+    allow(AmplitudeAPI).to receive(:track).with(kind_of(Array)).and_return(
+      double(:response, response_code: 200)
+    )
+  end
+
+  describe '#format' do
+    before do
+      amplitude.tag = tag
+      amplitude.emit(event, now)
+    end
+
+    context 'everything is set' do
+      let(:event) do
+        {
+          'user_id' => 42,
+          'uuid' => 'e6153b00-85d8-11e6-b1bc-43192d1e493f',
+          'first_name' => 'Bobby',
+          'last_name' => 'Weir',
+          'state' => 'CA',
+          'current_source' => 'fb_share',
+          'recruiter_id' => 710
+        }
+      end
+
+      let(:formatted_event) do
+        {
+          event_type: tag,
+          user_id: 42,
+          device_id: 'e6153b00-85d8-11e6-b1bc-43192d1e493f',
+          user_properties: {
+            first_name: 'Bobby',
+            last_name: 'Weir'
+          },
+          event_properties: {
+            current_source: 'fb_share'
+          }
+        }
+      end
+
+      it 'produces the expected output' do
+        amplitude.expect_format [tag, now, formatted_event].to_msgpack
+        amplitude.run
+      end
+    end
+
+    context 'the input only contains the user_id_key' do
+      let(:event) do
+        {
+          'user_id' => 42,
+          'first_name' => 'Bobby',
+          'last_name' => 'Weir',
+          'state' => 'CA',
+          'current_source' => 'fb_share',
+          'recruiter_id' => 710
+        }
+      end
+      let(:formatted_event) do
+        {
+          event_type: tag,
+          user_id: 42,
+          user_properties: {
+            first_name: 'Bobby',
+            last_name: 'Weir'
+          },
+          event_properties: {
+            current_source: 'fb_share'
+          }
+        }
+      end
+      it 'produces the expected output without device_id' do
+        amplitude.expect_format [tag, now, formatted_event].to_msgpack
+        amplitude.run
+      end
+    end
+
+    context 'the input only contains the device_id field' do
+      let(:event) do
+        {
+          'uuid' => 'e6153b00-85d8-11e6-b1bc-43192d1e493f',
+          'first_name' => 'Bobby',
+          'last_name' => 'Weir',
+          'state' => 'CA',
+          'current_source' => 'fb_share',
+          'recruiter_id' => 710
+        }
+      end
+      let(:formatted_event) do
+        {
+          event_type: tag,
+          device_id: 'e6153b00-85d8-11e6-b1bc-43192d1e493f',
+          user_properties: {
+            first_name: 'Bobby',
+            last_name: 'Weir'
+          },
+          event_properties: {
+            current_source: 'fb_share'
+          }
+        }
+      end
+      it 'produces the expected output without user_id' do
+        amplitude.expect_format [tag, now, formatted_event].to_msgpack
+        amplitude.run
+      end
+    end
+
+    context 'the input only contains neither user_id nor device_id field' do
+      let(:event) do
+        {
+          'first_name' => 'Bobby',
+          'last_name' => 'Weir',
+          'state' => 'CA',
+          'current_source' => 'fb_share',
+          'recruiter_id' => 710
+        }
+      end
+      it 'raises a Fluent::AmplitudeOutput::AmplitudeError' do
+        expect { amplitude.run }.to raise_error(
+          Fluent::AmplitudeOutput::AmplitudeError
+        )
+      end
+    end
+  end
+end
